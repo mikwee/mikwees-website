07d82da3b1df3c4f6a02e677bb47f4e9bcafd34d
mikwee <mikwee@protonmail.ch>
2021-10-14 19:56:16 +0300

    Background, title

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import {AwesomeButton} from 'react-awesome-button';
 import "react-awesome-button/dist/styles.css";
-import roots from './roots.json'
+import data from './roots.json'
 
 function App() {
   return (
     <div className="App">
-      <h1 id="title">{roots.title}</h1>
+      <div id="bg"></div>
+      <h1 id="title">{data.title}</h1>
+      {}
     </div>
   );
 }
