c839ee41b152ce64968c643a503163b7a6c2220d
mikwee <mikwee@protonmail.ch>
2021-10-15 18:58:37 +0300

    Starting to make the buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import {AwesomeButton} from 'react-awesome-button';
-import "react-awesome-button/dist/styles.css";
+import {AwesomeButton, AwesomeButtonSocial} from 'react-awesome-button';
+import AwesomeButtonStyle from "react-awesome-button/dist/styles.css";
 import data from './roots.json'
 
 function App() {
+  var res;
+  const socialTypes = ["facebook", "instagram", "twitter", "github", "youtube", "linkedin", "pinterest", "messenger", "whatsapp"];
+
   return (
     <div className="App">
-      <div id="bg"></div>
       <h1 id="title">{data.title}</h1>
-      {}
+      {data.links.map((rootDetail, index)=>{
+        if(socialTypes.includes(rootDetail.type)) {
+          res = <button className="roots">{rootDetail.text}</button>
+        }
+        else if (rootDetail.type === "basic") {
+          res = <button className="roots">{rootDetail.text}</button>
+        }
+        else {
+          // throw "One of your roots has an unrecognized type. Check out the documentation for the supported types.";
+        }
+        return res;
+      })}
     </div>
   );
 }
