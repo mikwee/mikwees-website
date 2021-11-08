39f46548a5f614b020c9548db1aba68cf322699e
mikwee <mikwee@protonmail.ch>
2021-11-08 20:47:58 +0200

    Adding the links

--- src/App.js
+++ src/App.js
@@ -15,34 +15,34 @@ function App() {
         <ButtonGroup vertical>
           {data.links.map((rootDetail, index)=>{
             if(rootDetail.type === "facebook") {
-              res = <Button id="facebook" variant="primary"><Facebook/> {rootDetail.text}</Button>
+              res = <Button id="facebook" variant="primary" href={rootDetail.url}><Facebook/> {rootDetail.text}</Button>
             }
             else if(rootDetail.type === "instagram") {
-              res = <Button id="instagram" variant="primary"><Instagram/> {rootDetail.text}</Button>
+              res = <Button id="instagram" variant="primary" href={rootDetail.url}><Instagram/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "twitter") {
-              res = <Button id="twitter" variant="primary"><Twitter/> {rootDetail.text}</Button>
+              res = <Button id="twitter" variant="primary" href={rootDetail.url}><Twitter/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "github") {
-              res = <Button id="github" variant="primary"><Github/> {rootDetail.text}</Button>
+              res = <Button id="github" variant="primary" href={rootDetail.url}><Github/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "youtube") {
-              res = <Button id="youtube" variant="primary"><Youtube/> {rootDetail.text}</Button>
+              res = <Button id="youtube" variant="primary" href={rootDetail.url}><Youtube/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "linkedin") {
-              res = <Button id="linkedin" variant="primary"><Linkedin/> {rootDetail.text}</Button>
+              res = <Button id="linkedin" variant="primary" href={rootDetail.url}><Linkedin/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "pinterest") {
-              res = <Button id="pinterest" variant="primary"><Pinterest/> {rootDetail.text}</Button>
+              res = <Button id="pinterest" variant="primary" href={rootDetail.url}><Pinterest/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "tumblr") {
-              res = <Button id="tumblr" variant="primary">{rootDetail.text}</Button>
+              res = <Button id="tumblr" variant="primary" href={rootDetail.url}>{rootDetail.text}</Button>
             }
             else if (rootDetail.type === "reddit") {
-              res = <Button id="reddit" variant="primary"><Reddit/> {rootDetail.text}</Button>
+              res = <Button id="reddit" variant="primary" href={rootDetail.url}><Reddit/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "basic") {
-              res = <Button variant="primary">{rootDetail.text}</Button>
+              res = <Button variant="primary" href={rootDetail.url}>{rootDetail.text}</Button>
             }
             else {
               // throw "One of your roots has an unrecognized type. Check out the documentation for the supported types.";
