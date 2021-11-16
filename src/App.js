4c9665633c9d433d7a4785b384385d8f198f272d
mikwee <mikwee@protonmail.ch>
2021-11-16 19:36:07 +0200

    Removed classes

--- src/App.js
+++ src/App.js
@@ -19,31 +19,31 @@ function App() {
               res = <Button id="facebook" variant="primary" href={rootDetail.url}><Facebook/> {rootDetail.text}</Button>
             }
             else if(rootDetail.type === "instagram") {
-              res = <Button className="roots" id="instagram" variant="primary" href={rootDetail.url}><Instagram/> {rootDetail.text}</Button>
+              res = <Button id="instagram" variant="primary" href={rootDetail.url}><Instagram/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "twitter") {
-              res = <Button className="roots" id="twitter" variant="primary" href={rootDetail.url}><Twitter/> {rootDetail.text}</Button>
+              res = <Button id="twitter" variant="primary" href={rootDetail.url}><Twitter/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "github") {
-              res = <Button className="roots" id="github" variant="primary" href={rootDetail.url}><Github/> {rootDetail.text}</Button>
+              res = <Button id="github" variant="primary" href={rootDetail.url}><Github/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "youtube") {
-              res = <Button className="roots" id="youtube" variant="primary" href={rootDetail.url}><Youtube/> {rootDetail.text}</Button>
+              res = <Button id="youtube" variant="primary" href={rootDetail.url}><Youtube/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "linkedin") {
-              res = <Button className="roots" id="linkedin" variant="primary" href={rootDetail.url}><Linkedin/> {rootDetail.text}</Button>
+              res = <Button id="linkedin" variant="primary" href={rootDetail.url}><Linkedin/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "pinterest") {
-              res = <Button className="roots" id="pinterest" variant="primary" href={rootDetail.url}><Pinterest/> {rootDetail.text}</Button>
+              res = <Button id="pinterest" variant="primary" href={rootDetail.url}><Pinterest/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "tumblr") {
-              res = <Button className="roots" id="tumblr" variant="primary" href={rootDetail.url}><Tumblr viewBox="250 0 300 200" width="32px" height="32px"/> {rootDetail.text}</Button>
+              res = <Button id="tumblr" variant="primary" href={rootDetail.url}><Tumblr viewBox="250 0 300 200" width="32px" height="32px"/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "reddit") {
-              res = <Button className="roots" id="reddit" variant="primary" href={rootDetail.url}><Reddit/> {rootDetail.text}</Button>
+              res = <Button id="reddit" variant="primary" href={rootDetail.url}><Reddit/> {rootDetail.text}</Button>
             }
             else if (rootDetail.type === "basic") {
-              res = <Button className="roots" variant="primary" href={rootDetail.url}>{rootDetail.text}</Button>
+              res = <Button variant="primary" href={rootDetail.url}>{rootDetail.text}</Button>
             }
             else {
               // throw "One of your roots has an unrecognized type. Check out the documentation for the supported types.";
